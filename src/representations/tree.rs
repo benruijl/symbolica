8c2741c780c56edd0ecfd654374e12ab6e2810bf
adamnemecek <adamnemecek@gmail.com>
2023-07-04 17:39:57 -0700

    nitpicks

--- src/representations/tree.rs
+++ src/representations/tree.rs
@@ -5,21 +5,21 @@ use super::{number::Number, Identifier};
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum AtomTree {
     Var(Identifier),
-    Fn(Identifier, Vec<AtomTree>), // name and args
+    Fn(Identifier, Vec<Self>), // name and args
     Num(Number),
-    Pow(Box<(AtomTree, AtomTree)>),
-    Mul(Vec<AtomTree>),
-    Add(Vec<AtomTree>),
+    Pow(Box<(Self, Self)>),
+    Mul(Vec<Self>),
+    Add(Vec<Self>),
 }
 
 impl AtomTree {
     pub fn len(&self) -> usize {
-        size_of::<AtomTree>()
+        size_of::<Self>()
             + match self {
-                AtomTree::Fn(_, args) | AtomTree::Mul(args) | AtomTree::Add(args) => {
+                Self::Fn(_, args) | Self::Mul(args) | Self::Add(args) => {
                     args.iter().map(|a| a.len()).sum()
                 }
-                AtomTree::Pow(p) => p.0.len() + p.1.len(),
+                Self::Pow(p) => p.0.len() + p.1.len(),
                 _ => 0,
             }
     }

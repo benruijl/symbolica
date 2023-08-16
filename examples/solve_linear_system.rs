54359406a050f4bfcee1717a678aba371b4dbdeb
Ben Ruijl <benruyl@gmail.com>
2023-08-16 17:44:37 +0200

    Add example that shows how to solve a linear system with rational polynomial coefficients

---
+++ examples/solve_linear_system.rs
@@ -0,0 +1,98 @@
+use symbolica::{
+    parser::parse,
+    printer::{PrintMode, RationalPolynomialPrinter},
+    representations::default::DefaultRepresentation,
+    rings::{
+        integer::IntegerRing,
+        linear_system::Matrix,
+        rational::RationalField,
+        rational_polynomial::{RationalPolynomial, RationalPolynomialField},
+    },
+    state::{State, Workspace},
+};
+
+fn main() {
+    let mut state = State::new();
+    let workspace = Workspace::<DefaultRepresentation>::new();
+
+    let system = [["c", "c+1", "c^2+5"], ["1", "c", "c+1"], ["c-1", "-1", "c"]];
+    let rhs = ["1", "2", "-1"];
+
+    println!("Solve linear system in x\u{20D7}:");
+    for (r, v) in system.iter().zip(&rhs) {
+        println!("\t ({}).x\u{20D7} = {}", r.join(","), v);
+    }
+
+    let var_map = vec![state.get_or_insert_var("c")];
+
+    let system_rat: Vec<RationalPolynomial<IntegerRing, u8>> = system
+        .iter()
+        .flatten()
+        .map(|s| {
+            parse(s)
+                .unwrap()
+                .to_atom(&mut state, &workspace)
+                .unwrap()
+                .to_view()
+                .to_rational_polynomial(
+                    &workspace,
+                    &state,
+                    RationalField::new(),
+                    IntegerRing::new(),
+                    Some(&var_map),
+                )
+                .unwrap()
+        })
+        .collect();
+
+    let rhs_rat: Vec<RationalPolynomial<IntegerRing, u8>> = rhs
+        .iter()
+        .map(|s| {
+            parse(s)
+                .unwrap()
+                .to_atom(&mut state, &workspace)
+                .unwrap()
+                .to_view()
+                .to_rational_polynomial(
+                    &workspace,
+                    &state,
+                    RationalField::new(),
+                    IntegerRing::new(),
+                    Some(&var_map),
+                )
+                .unwrap()
+        })
+        .collect();
+
+    let m = Matrix {
+        shape: (system.len() as u32, system.len() as u32),
+        data: system_rat.into(),
+        field: RationalPolynomialField::new(IntegerRing::new()),
+    };
+    let b = Matrix {
+        shape: (rhs.len() as u32, 1),
+        data: rhs_rat.into(),
+        field: RationalPolynomialField::new(IntegerRing::new()),
+    };
+
+    match m.solve(&b) {
+        Ok(sol) => {
+            println!(
+                "x\u{20D7} = {{{}}}",
+                sol.data
+                    .iter()
+                    .map(|r| format!(
+                        "{}",
+                        RationalPolynomialPrinter {
+                            poly: &r,
+                            state: &state,
+                            print_mode: PrintMode::default(),
+                        }
+                    ))
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+        }
+        Err(e) => panic!("Could not solve {:?}", e),
+    }
+}

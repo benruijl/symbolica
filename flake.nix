3649baaa0369639dfea9bf6b593a0096f6c934d6
lcnbr <im@lcnbr.ch>
2023-10-06 15:24:06 +0200

    added nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              packages = [
+                pkgs.gnum4
+                pkgs.gmp
+                pkgs.mpfr
+                pkgs.gnumake
+                pkgs.diffutils
+                pkgs.glibc
+              ];
+
+              languages.rust.enable = true;
+              languages.cplusplus.enable = true;
+            }
+          ];
+        };
+      });
+  };
+}

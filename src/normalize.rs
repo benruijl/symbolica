2e620a0c566a64b655ed5fd9799f810c366a392a
Adam Nemecek <adamnemecek@gmail.com>
2023-12-29 09:25:49 -0800

    use Self

--- src/normalize.rs
+++ src/normalize.rs
@@ -20,22 +20,20 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
         }
 
         match (&self, other) {
-            (AtomView::Num(n1), AtomView::Num(n2)) => {
-                n1.get_number_view().cmp(&n2.get_number_view())
-            }
-            (AtomView::Num(_), _) => Ordering::Greater,
+            (Self::Num(n1), AtomView::Num(n2)) => n1.get_number_view().cmp(&n2.get_number_view()),
+            (Self::Num(_), _) => Ordering::Greater,
             (_, AtomView::Num(_)) => Ordering::Less,
-            (AtomView::Var(v1), AtomView::Var(v2)) => v1.get_name().cmp(&v2.get_name()),
-            (AtomView::Var(_), _) => Ordering::Less,
+            (Self::Var(v1), AtomView::Var(v2)) => v1.get_name().cmp(&v2.get_name()),
+            (Self::Var(_), _) => Ordering::Less,
             (_, AtomView::Var(_)) => Ordering::Greater,
-            (AtomView::Pow(p1), AtomView::Pow(p2)) => {
+            (Self::Pow(p1), AtomView::Pow(p2)) => {
                 let (b1, e1) = p1.get_base_exp();
                 let (b2, e2) = p2.get_base_exp();
                 b1.cmp(&b2).then_with(|| e1.cmp(&e2))
             }
             (_, AtomView::Pow(_)) => Ordering::Greater,
-            (AtomView::Pow(_), _) => Ordering::Less,
-            (AtomView::Mul(m1), AtomView::Mul(m2)) => {
+            (Self::Pow(_), _) => Ordering::Less,
+            (Self::Mul(m1), AtomView::Mul(m2)) => {
                 let it1 = m1.to_slice();
                 let it2 = m2.to_slice();
 
@@ -53,9 +51,9 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
 
                 Ordering::Equal
             }
-            (AtomView::Mul(_), _) => Ordering::Less,
+            (Self::Mul(_), _) => Ordering::Less,
             (_, AtomView::Mul(_)) => Ordering::Greater,
-            (AtomView::Add(a1), AtomView::Add(a2)) => {
+            (Self::Add(a1), AtomView::Add(a2)) => {
                 let it1 = a1.to_slice();
                 let it2 = a2.to_slice();
 
@@ -73,10 +71,10 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
 
                 Ordering::Equal
             }
-            (AtomView::Add(_), _) => Ordering::Less,
+            (Self::Add(_), _) => Ordering::Less,
             (_, AtomView::Add(_)) => Ordering::Greater,
 
-            (AtomView::Fun(f1), AtomView::Fun(f2)) => {
+            (Self::Fun(f1), AtomView::Fun(f2)) => {
                 let name_comp = f1.get_name().cmp(&f2.get_name());
                 if name_comp != Ordering::Equal {
                     return name_comp;
@@ -106,12 +104,12 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
     /// Compare factors in a term. `x` and `x^2` are placed next to each other by sorting a power based on the base only.
     pub(crate) fn cmp_factors(&self, other: &AtomView<'_, P>) -> Ordering {
         match (&self, other) {
-            (AtomView::Num(_), AtomView::Num(_)) => Ordering::Equal,
-            (AtomView::Num(_), _) => Ordering::Greater,
+            (Self::Num(_), AtomView::Num(_)) => Ordering::Equal,
+            (Self::Num(_), _) => Ordering::Greater,
             (_, AtomView::Num(_)) => Ordering::Less,
 
-            (AtomView::Var(v1), AtomView::Var(v2)) => v1.get_name().cmp(&v2.get_name()),
-            (AtomView::Pow(p1), AtomView::Pow(p2)) => {
+            (Self::Var(v1), AtomView::Var(v2)) => v1.get_name().cmp(&v2.get_name()),
+            (Self::Pow(p1), AtomView::Pow(p2)) => {
                 // TODO: inline partial_cmp call by creating an inlined version
                 p1.get_base().cmp(&p2.get_base())
             }
@@ -119,17 +117,17 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
                 let base = p2.get_base();
                 self.cmp(&base).then(Ordering::Less) // sort x^2*x -> x*x^2
             }
-            (AtomView::Pow(p1), _) => {
+            (Self::Pow(p1), _) => {
                 let base = p1.get_base();
                 base.cmp(other).then(Ordering::Greater)
             }
-            (AtomView::Var(_), _) => Ordering::Less,
+            (Self::Var(_), _) => Ordering::Less,
             (_, AtomView::Var(_)) => Ordering::Greater,
 
-            (AtomView::Mul(_), _) | (_, AtomView::Mul(_)) => {
+            (Self::Mul(_), _) | (_, AtomView::Mul(_)) => {
                 unreachable!("Cannot have a submul in a factor");
             }
-            (AtomView::Add(a1), AtomView::Add(a2)) => {
+            (Self::Add(a1), AtomView::Add(a2)) => {
                 let it1 = a1.to_slice();
                 let it2 = a2.to_slice();
 
@@ -147,10 +145,10 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
 
                 Ordering::Equal
             }
-            (AtomView::Add(_), _) => Ordering::Less,
+            (Self::Add(_), _) => Ordering::Less,
             (_, AtomView::Add(_)) => Ordering::Greater,
 
-            (AtomView::Fun(f1), AtomView::Fun(f2)) => {
+            (Self::Fun(f1), AtomView::Fun(f2)) => {
                 let name_comp = f1.get_name().cmp(&f2.get_name());
                 if name_comp != Ordering::Equal {
                     return name_comp;
@@ -182,17 +180,17 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
         debug_assert!(!matches!(self, AtomView::Add(_)));
         debug_assert!(!matches!(other, AtomView::Add(_)));
         match (self, other) {
-            (AtomView::Num(_), AtomView::Num(_)) => Ordering::Equal,
-            (AtomView::Num(_), _) => Ordering::Greater,
+            (Self::Num(_), AtomView::Num(_)) => Ordering::Equal,
+            (Self::Num(_), _) => Ordering::Greater,
             (_, AtomView::Num(_)) => Ordering::Less,
 
-            (AtomView::Var(v1), AtomView::Var(v2)) => v1.get_name().cmp(&v2.get_name()),
-            (AtomView::Pow(p1), AtomView::Pow(p2)) => {
+            (Self::Var(v1), AtomView::Var(v2)) => v1.get_name().cmp(&v2.get_name()),
+            (Self::Pow(p1), AtomView::Pow(p2)) => {
                 let (b1, e1) = p1.get_base_exp();
                 let (b2, e2) = p2.get_base_exp();
                 b1.cmp(&b2).then_with(|| e1.cmp(&e2))
             }
-            (AtomView::Mul(m1), AtomView::Mul(m2)) => {
+            (Self::Mul(m1), AtomView::Mul(m2)) => {
                 let actual_len1 = if m1.has_coefficient() {
                     m1.get_nargs() - 1
                 } else {
@@ -226,7 +224,7 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
 
                 Ordering::Equal
             }
-            (AtomView::Mul(m1), a2) => {
+            (Self::Mul(m1), a2) => {
                 if !m1.has_coefficient() || m1.get_nargs() != 2 {
                     return Ordering::Greater;
                 }
@@ -242,12 +240,12 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
                 let it2 = m2.to_slice();
                 a1.cmp(&it2.get(0))
             }
-            (AtomView::Var(_), _) => Ordering::Less,
+            (Self::Var(_), _) => Ordering::Less,
             (_, AtomView::Var(_)) => Ordering::Greater,
             (_, AtomView::Pow(_)) => Ordering::Greater,
-            (AtomView::Pow(_), _) => Ordering::Less,
+            (Self::Pow(_), _) => Ordering::Less,
 
-            (AtomView::Fun(f1), AtomView::Fun(f2)) => {
+            (Self::Fun(f1), AtomView::Fun(f2)) => {
                 let name_comp = f1.get_name().cmp(&f2.get_name());
                 if name_comp != Ordering::Equal {
                     return name_comp;
@@ -271,7 +269,7 @@ impl<'a, P: AtomSet> AtomView<'a, P> {
                     f1.fast_cmp(*f2)
                 }
             }
-            (AtomView::Add(_), _) | (_, AtomView::Add(_)) => unreachable!("Cannot have nested add"),
+            (Self::Add(_), _) | (_, AtomView::Add(_)) => unreachable!("Cannot have nested add"),
         }
     }
 }

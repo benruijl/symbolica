9ba552aee9bc13f33328e12226e8b9078f421a11
Ben Ruijl <benruyl@gmail.com>
2024-08-24 15:02:59 +0200

    Add support for directed edges
    - Take directed edges into account during refinement
    - Improve performance of refinement

--- src/graph.rs
+++ src/graph.rs
@@ -5,18 +5,27 @@ use std::{
     hash::Hash,
 };
 
+/// A node in a graph, with arbitrary data.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Node<NodeData = Empty> {
-    data: NodeData,
-    edges: Vec<usize>,
+    /// Arbitrary data associated with the node.
+    pub data: NodeData,
+    /// Indices of the edges connected to the node.
+    pub edges: Vec<usize>,
 }
 
+/// An edge in a graph, with arbitrary data.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Edge<EdgeData = Empty> {
-    vertices: (usize, usize),
-    data: EdgeData,
+    /// Indices of the vertices connected by the edge.
+    pub vertices: (usize, usize),
+    /// If `true`, the edge is directed from `vertices.0` to `vertices.1`.
+    pub directed: bool,
+    /// Arbitrary data associated with the edge.
+    pub data: EdgeData,
 }
 
+/// Empty data type.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Empty;
 
@@ -40,7 +49,11 @@ impl<N: Display, E: Display> std::fmt::Display for Graph<N, E> {
         }
 
         for x in &self.edges {
-            writeln!(f, "{} {}--{}", x.data, x.vertices.0, x.vertices.1)?;
+            if x.directed {
+                writeln!(f, "{} {}->{}", x.data, x.vertices.0, x.vertices.1)?;
+            } else {
+                writeln!(f, "{} {}--{}", x.data, x.vertices.0, x.vertices.1)?;
+            }
         }
         Ok(())
     }
@@ -66,10 +79,12 @@ impl<N, E> Graph<N, E> {
     }
 
     /// Add an edge between vertex indices `source` and `target` to the graph, with arbitrary data.
-    pub fn add_edge(&mut self, source: usize, target: usize, data: E) {
+    /// If `directed` is true, the edge is directed from `source` to `target`.
+    pub fn add_edge(&mut self, source: usize, target: usize, directed: bool, data: E) {
         let index = self.edges.len();
         self.edges.push(Edge {
             vertices: (source, target),
+            directed,
             data,
         });
         self.nodes[source].edges.push(index);
@@ -77,39 +92,46 @@ impl<N, E> Graph<N, E> {
     }
 
     /// Get the node with index `index`.
+    #[inline(always)]
     pub fn node(&self, index: usize) -> &Node<N> {
         &self.nodes[index]
     }
 
     /// Get the edge with index `index`.
+    #[inline(always)]
     pub fn edge(&self, index: usize) -> &Edge<E> {
         &self.edges[index]
     }
 
     /// Get all nodes of the graph.
+    #[inline(always)]
     pub fn nodes(&self) -> &[Node<N>] {
         &self.nodes
     }
 
     /// Get all edges of the graph.
+    #[inline(always)]
     pub fn edges(&self) -> &[Edge<E>] {
         &self.edges
     }
 }
 
 impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq + Hash> Graph<N, E> {
-    /// Canonize the graph using McKay's canonical graph labeling algorithm.
-    pub fn canonize(&self) -> Self {
+    /// Canonize the graph using McKay's canonical graph labeling algorithm,
+    /// returning the vertex mapping and the canonical form.
+    pub fn canonize(&self) -> (Vec<usize>, Self) {
         if self.nodes.len() <= u16::MAX as usize {
-            self.canonize_impl::<u16>()
+            let r = self.canonize_impl::<u16>();
+            (r.0.into_iter().map(|x| x as usize).collect(), r.1)
         } else if self.nodes.len() <= u32::MAX as usize {
-            self.canonize_impl::<u32>()
+            let r = self.canonize_impl::<u32>();
+            (r.0.into_iter().map(|x| x as usize).collect(), r.1)
         } else {
             self.canonize_impl::<usize>()
         }
     }
 
-    fn canonize_impl<I: NodeIndex>(&self) -> Self {
+    fn canonize_impl<I: NodeIndex>(&self) -> (Vec<I>, Self) {
         // split on vertex color
         let mut h = HashMap::default();
         for (i, x) in self.nodes.iter().enumerate() {
@@ -130,7 +152,7 @@ impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq +
 
         let mut automorphisms = vec![];
         let mut leaf_nodes: HashMap<_, (Vec<_>, Vec<_>)> = HashMap::default(); // TODO: limit growth
-        let mut current_best: Option<(Graph<&N, &E>, Vec<Invariant<I>>)> = None;
+        let mut current_best: Option<(Graph<&N, &E>, Vec<I>, Vec<Invariant<I>>)> = None;
 
         let mut node_buffer = vec![];
 
@@ -139,7 +161,7 @@ impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq +
                 node.refine(self);
             }
 
-            if let Some((_best, best_invariant)) = &current_best {
+            if let Some((_, _, best_invariant)) = &current_best {
                 // the canonical form is defined as the maximal isomorphism, prepended with the node invariants of the path
                 // at each tree level, the node invariant must therefore be at least as good as the best
                 // to be a potential canonical form
@@ -177,15 +199,15 @@ impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq +
                         .position(|&x| x.to_usize() == e.vertices.1)
                         .unwrap();
 
-                    if a < b {
-                        edges.push((a, b, &e.data));
+                    if e.directed || a <= b {
+                        edges.push((a, b, e.directed, &e.data));
                     } else {
-                        edges.push((b, a, &e.data));
+                        edges.push((b, a, e.directed, &e.data));
                     }
                 }
                 edges.sort();
-                for x in edges {
-                    g.add_edge(x.0, x.1, x.2);
+                for (v1, v2, dir, d) in edges {
+                    g.add_edge(v1, v2, dir, d);
                 }
 
                 let path: Vec<_> = stack.iter().map(|x| x.selected_vertex.unwrap()).collect();
@@ -244,7 +266,7 @@ impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq +
                     continue;
                 }
 
-                if let Some((best, _)) = &current_best {
+                if let Some((best, _, _)) = &current_best {
                     debug_assert!(g != *best);
                     if g < *best {
                         // isomorph does not improve the best
@@ -259,7 +281,7 @@ impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq +
                 let mut best_invariant: Vec<_> =
                     stack.iter().map(|x| x.invariant.clone()).collect();
                 best_invariant.push(node.invariant.clone());
-                current_best = Some((g.clone(), best_invariant));
+                current_best = Some((g.clone(), partition.clone(), best_invariant));
                 leaf_nodes.insert(g, (partition, path));
                 node_buffer.push(node);
             } else {
@@ -345,15 +367,41 @@ impl<N: Clone + PartialOrd + Ord + Eq + Hash, E: Clone + PartialOrd + Ord + Eq +
             }
         }
 
-        let (best, _) = current_best.unwrap();
+        let (best, map, _) = current_best.unwrap();
         let mut g = Graph::new();
         for i in 0..best.nodes.len() {
             g.add_node(best.node(i).data.clone());
         }
         for e in &best.edges {
-            g.add_edge(e.vertices.0, e.vertices.1, e.data.clone());
+            g.add_edge(e.vertices.0, e.vertices.1, e.directed, e.data.clone());
         }
-        g
+
+        (map, g)
+    }
+
+    /// Returns `true` iff the graph is isomorphic to `other`.
+    pub fn is_isomorphic(&self, other: &Self) -> bool {
+        if self.nodes.len() != other.nodes.len() || self.edges.len() != other.edges.len() {
+            return false;
+        }
+
+        if self == other {
+            return true;
+        }
+
+        // check if the first refinement has the same node invariant
+        let mut node = SearchTreeNode::<usize>::default();
+        node.partition = vec![vec![0; self.nodes.len()]];
+        node.refine(self);
+        let mut other_node = SearchTreeNode::default();
+        other_node.partition = vec![vec![0; other.nodes.len()]];
+        other_node.refine(other);
+
+        if node.invariant != other_node.invariant {
+            return false;
+        }
+
+        self.canonize().1 == other.canonize().1
     }
 }
 
@@ -365,33 +413,39 @@ trait NodeIndex: Default + Copy + PartialOrd + Ord + Eq + Hash + Display + Debug
 }
 
 impl NodeIndex for usize {
+    #[inline(always)]
     fn to_usize(&self) -> usize {
         *self
     }
 
+    #[inline(always)]
     fn from_usize(x: usize) -> Self {
         x
     }
 }
 
 impl NodeIndex for u32 {
+    #[inline(always)]
     fn to_usize(&self) -> usize {
         *self as usize
     }
 
+    #[inline(always)]
     fn from_usize(x: usize) -> Self {
-        assert!(x <= u32::MAX as usize);
+        debug_assert!(x <= u32::MAX as usize);
         x as u32
     }
 }
 
 impl NodeIndex for u16 {
+    #[inline(always)]
     fn to_usize(&self) -> usize {
         *self as usize
     }
 
+    #[inline(always)]
     fn from_usize(x: usize) -> Self {
-        assert!(x <= u16::MAX as usize);
+        debug_assert!(x <= u16::MAX as usize);
         x as u16
     }
 }
@@ -422,8 +476,13 @@ impl<I: NodeIndex> SearchTreeNode<I> {
             .extend(self.partition.iter().map(|x| I::from_usize(x.len())));
     }
 
-    /// Apply equitable coloring rules and set the node invariant.
+    /// Apply a label-invariant refinement to the partition.
+    /// The current refinement is based on the signature of the neighbors of a vertex.
+    /// The signature takes directional edges and edge colors into account.
     fn refine<N, E: PartialOrd + Ord>(&mut self, g: &Graph<N, E>) {
+        let largest_partition = self.partition.iter().map(|x| x.len()).max().unwrap();
+        let mut degrees = vec![(vec![], I::from_usize(0)); largest_partition];
+
         'next: loop {
             for (ii, i) in self.partition.iter().enumerate() {
                 if i.len() == 1 {
@@ -433,46 +492,37 @@ impl<I: NodeIndex> SearchTreeNode<I> {
                 for j in &self.partition {
                     // sorted edge colors of edges in i that connect to vertices in j
                     // the length of this vector is the degree of the vertex i in j
-                    let mut degrees: Vec<_> = i
-                        .iter()
-                        .map(|v| {
-                            (
-                                {
-                                    let mut edge_data: Vec<_> = g
-                                        .node(v.to_usize())
-                                        .edges
-                                        .iter()
-                                        .filter_map(|&e| {
-                                            let e = g.edge(e);
-                                            let k = if e.vertices.0 == v.to_usize() {
-                                                NodeIndex::from_usize(e.vertices.1)
-                                            } else {
-                                                NodeIndex::from_usize(e.vertices.0)
-                                            };
-                                            if j.contains(&k) {
-                                                Some(&e.data)
-                                            } else {
-                                                None
-                                            }
-                                        })
-                                        .collect();
-                                    edge_data.sort();
-                                    edge_data
-                                },
-                                *v,
-                            )
-                        })
-                        .collect();
+                    for ((edge_data, vert), v) in degrees.iter_mut().zip(i) {
+                        edge_data.clear();
 
-                    if degrees.windows(2).all(|w| w[0].0 == w[1].0) {
+                        for e in &g.node(v.to_usize()).edges {
+                            let e = g.edge(*e);
+                            let (k, is_source) = if e.vertices.0 == v.to_usize() {
+                                (NodeIndex::from_usize(e.vertices.1), true)
+                            } else {
+                                (NodeIndex::from_usize(e.vertices.0), false)
+                            };
+                            if j.contains(&k) {
+                                if e.directed {
+                                    edge_data.push((&e.data, is_source));
+                                } else {
+                                    edge_data.push((&e.data, true));
+                                }
+                            }
+                        }
+                        edge_data.sort();
+                        *vert = *v;
+                    }
+
+                    if degrees[..i.len()].windows(2).all(|w| w[0].0 == w[1].0) {
                         continue;
                     }
 
-                    degrees.sort();
+                    degrees[..i.len()].sort();
 
                     let mut degs = vec![];
                     let mut cur = vec![degrees[0].1];
-                    for x in degrees.windows(2) {
+                    for x in degrees[..i.len()].windows(2) {
                         if x[0].0 != x[1].0 {
                             degs.push(cur);
                             cur = vec![x[1].1];
@@ -494,37 +544,69 @@ impl<I: NodeIndex> SearchTreeNode<I> {
     }
 }
 
-#[test]
-fn canonize() {
-    let mut g = Graph::new();
-    let n0 = g.add_node(1);
-    let n1 = g.add_node(0);
-    let n2 = g.add_node(1);
-    let n3 = g.add_node(0);
-    let n4 = g.add_node(2);
-    let n5 = g.add_node(0);
-    let n6 = g.add_node(1);
-    let n7 = g.add_node(0);
-    let n8 = g.add_node(1);
+#[cfg(test)]
+mod test {
+    use crate::graph::{Graph, SearchTreeNode};
 
-    g.add_edge(n0, n1, 0);
-    g.add_edge(n0, n3, 0);
-    g.add_edge(n1, n2, 0);
-    g.add_edge(n1, n3, 0);
-    g.add_edge(n1, n4, 0);
-    g.add_edge(n1, n5, 0);
-    g.add_edge(n2, n5, 0);
-    g.add_edge(n3, n4, 0);
-    g.add_edge(n3, n6, 0);
-    g.add_edge(n3, n7, 0);
-    g.add_edge(n4, n5, 0);
-    g.add_edge(n4, n7, 0);
-    g.add_edge(n5, n7, 0);
-    g.add_edge(n5, n8, 0);
-    g.add_edge(n6, n7, 0);
-    g.add_edge(n7, n8, 0);
+    #[test]
+    fn directed() {
+        let mut g = Graph::new();
+        let n0 = g.add_node(0);
+        let n1 = g.add_node(0);
+        g.add_edge(n0, n1, false, 0);
+        g.add_edge(n0, n1, false, 1);
 
-    let c = g.canonize();
+        let mut node = SearchTreeNode::<usize>::default();
+        node.partition = vec![vec![0, 1]];
+        node.refine(&g);
+        assert_eq!(node.partition.len(), 1); // cannot refine
 
-    assert_eq!(c.edge(0).vertices, (0, 2));
+        let mut g = Graph::new();
+        let n0 = g.add_node(0);
+        let n1 = g.add_node(0);
+        g.add_edge(n0, n1, true, 0);
+        g.add_edge(n0, n1, false, 1);
+
+        let mut node = SearchTreeNode::<usize>::default();
+        node.partition = vec![vec![0, 1]];
+        node.refine(&g);
+        assert_eq!(node.partition.len(), 2); // distinguish two nodes based on directed edge
+    }
+
+    #[test]
+    fn canonize() {
+        let mut g = Graph::new();
+        let n0 = g.add_node(1);
+        let n1 = g.add_node(0);
+        let n2 = g.add_node(1);
+        let n3 = g.add_node(0);
+        let n4 = g.add_node(2);
+        let n5 = g.add_node(0);
+        let n6 = g.add_node(1);
+        let n7 = g.add_node(0);
+        let n8 = g.add_node(1);
+
+        g.add_edge(n0, n1, false, 0);
+        g.add_edge(n0, n3, false, 0);
+        g.add_edge(n1, n2, false, 0);
+        g.add_edge(n1, n3, false, 0);
+        g.add_edge(n1, n4, false, 0);
+        g.add_edge(n1, n5, false, 0);
+        g.add_edge(n2, n5, false, 0);
+        g.add_edge(n3, n4, false, 0);
+        g.add_edge(n3, n6, false, 0);
+        g.add_edge(n3, n7, false, 0);
+        g.add_edge(n4, n5, false, 0);
+        g.add_edge(n4, n7, false, 0);
+        g.add_edge(n5, n7, false, 0);
+        g.add_edge(n5, n8, false, 0);
+        g.add_edge(n6, n7, false, 0);
+        g.add_edge(n7, n8, false, 0);
+
+        let c = g.canonize();
+
+        println!("{:?}\n{}", c.0, c.1);
+
+        assert_eq!(c.1.edge(0).vertices, (0, 2));
+    }
 }

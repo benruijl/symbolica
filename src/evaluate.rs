9effee8848e77af91f0078a5ec3d961fb965dcff
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2025-05-05 14:23:52 +0200

    Add vec_ prefixed cpp code that evaluates multiple values

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1515,6 +1515,9 @@ impl<T: ExportNumber + SingleFloat> ExpressionEvaluator<T> {
             function_name
         );
 
+        res += &format!("\nextern \"C\" {{\n\tvoid vec_{0}_double(double *params, double *buffer, double *out, int n) {{\n\t\tfor (int j = 0; j < n ; j++) {{ {0}_double(params + {1}*j, buffer, out + j); }}\n\t}}\n}}\n", function_name, self.param_count);
+        res += &format!("\nextern \"C\" {{\n\tvoid vec_{0}_complex(std::complex<double> *params, std::complex<double> *buffer,  std::complex<double> *out, int n) {{\n\t\tfor (int j = 0; j < n ; j++) {{ {0}_double(params + {1}*j, buffer, out + j); }}\n\t}}\n}}\n", function_name, self.param_count);
+
         res
     }
 
